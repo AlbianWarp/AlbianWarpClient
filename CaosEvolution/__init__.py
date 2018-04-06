7e91ca4edc84f432c54178b05d28855da02788d6
KeyboardInterrupt <KeyboardInterrupt@KeyboardInterrupt.com>
2018-04-06 16:46:10 -0700

    async rewrite

--- CaosEvolution/__init__.py
+++ CaosEvolution/__init__.py
@@ -1,7 +1,7 @@
 import sys
 import clr
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 sys.path.append('.')
 clr.AddReference('CAOS')
 from CAOS import *
@@ -19,4 +19,22 @@ class LoggingCaosInjector(CaosInjector):
         logging.debug(result.Content)
         return result
 
+
 CI = LoggingCaosInjector('Docking Station')
+
+
+@property
+def WorldName():
+    return CI.ExecuteCaos('outs wnam').Content.strip('\x00')
+
+
+def add_user_to_contact_list(username):
+    CI.ExecuteCaos('rtar 1 1 157 mesg wrt+ targ 1000 "%s" 0 0' % username)
+
+
+def _ds_gui_online():
+    CI.ExecuteCaos("enum 1 2 14 mesg writ targ 1005 mesg writ targ 500 next")
+
+
+def _ds_gui_offline():
+    CI.ExecuteCaos("enum 1 2 14 mesg writ targ 1005 mesg writ targ 501 next")
