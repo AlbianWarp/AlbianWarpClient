7491bb521fa899b3d8669d887eb87a4f2a7e8b9b
KeyboardInterrupt <KeyboardInterrupt@KeyboardInterrupt.com>
2018-04-24 14:30:37 -0700

    changes for Beta Baboon compat.

--- AlbianWarpPythonClient.py
+++ AlbianWarpPythonClient.py
@@ -1,15 +1,21 @@
+import os
+import ssl
+import time
+import json
+import logging
+import requests
+import threading
+import configparser
+
+from urllib3 import PoolManager
+from requests.adapters import HTTPAdapter
+
+from util import retry
 from CaosEvolution.GameVariables import *
 from CaosEvolution.Agent import AgentBuilder, enumAgents
 from CaosEvolution import CI, WorldName, add_user_to_contact_list, delete_creature_by_moniker
-from decorators import retry
-import requests
-from requests.auth import HTTPBasicAuth
-import logging
-import os
-import configparser
-from socketIO_client import SocketIO, BaseNamespace
-import threading
-import time
+
+from ws4py.client.threadedclient import WebSocketClient
 
 logger = logging.getLogger('AW')
 logger.setLevel("INFO")
@@ -23,17 +29,27 @@ def read_config():
     config['DEFAULT'] = {
         'username': '',
         'password': '',
+        'websocket_url': 'wss://alpha.albianwarp.com/ws',
         'url': 'https://alpha.albianwarp.com',
-        'port': 443,
         'my_creatures_directory': r"C:\Users\%s\Documents\Creatures\Docking Station\My Creatures" % os.getlogin()
     }
     config.read('albianwarp.cfg')
     return config['albianwarp']
 
 
+class MyAdapter(HTTPAdapter):
+
+    def init_poolmanager(self, connections, maxsize, block=False, **kwargs):
+        self.poolmanager = PoolManager(num_pools=connections,
+                                       maxsize=maxsize,
+                                       block=block,
+                                       ssl_version=ssl.PROTOCOL_TLSv1_2)
+
+
+s = requests.Session()
+s.mount('https://', MyAdapter())
 cfg = read_config()
-socketIO = None
-socketio_sid = None
+ws = None
 auth_token = None
 run = True
 
@@ -45,43 +61,56 @@ def sleep_while_run(seconds):
         time.sleep(1)
 
 
-class AwNamespace(BaseNamespace):
-
-    def on_connect(self):
-        socketIO.emit('auth', {'token': auth_token})
-        print({'token': auth_token})
-
-    def on_reconnect(self):
-        socketIO.emit('auth', {'token': auth_token})
-        print({'token': auth_token})
-
-    def on_disconnect(self):
-        socketIO.emit('auth', {'token': auth_token})
-        print({'token': auth_token})
-
-    def on_auth_acc(self, data):
-        global socketio_sid
-        socketio_sid = data['sid']
-
-    def on_rtdma(self, data):
-        print("recieved a RTDMA")
-        try:
-            if AgentBuilder(1, 1, 35756, data).inject().Success:
-                print("DEBUG: INJECTED incoming RTDMA: %s" % data)
-        except Exception as e:
-            print("ERROR: %s" % e)
-        print(data)
+def consumer(message, websocket):
+    try:
+        data = json.loads(str(message))
+        if "ping" in data:
+            websocket.send(json.dumps({"pong": "%s" % data['ping']}))
+        if "aw_sender" in data:
+            print("received a RTDMA")
+            try:
+                if AgentBuilder(1, 1, 35756, data).inject().Success:
+                    print("DEBUG: INJECTED incoming RTDMA: %s" % data)
+            except Exception as e:
+                print("ERROR: %s" % e)
+    except Exception as e:
+        print(message)
+        print("NOT JSON! %s" % e)
 
 
-def socketio_worker():
-    global socketIO
-    print("DEBUG: soketio_handler thread started")
-    socketIO = SocketIO(cfg['url'], cfg['port'], Namespace=AwNamespace)
-    socketIO.emit('auth', {'token': auth_token})
-    socketIO.wait()
+class AwSocketClient(WebSocketClient):
+
+    def opened(self):
+        self.send(json.dumps({"auth": auth_token}))
+
+    def closed(self, code, reason=None):
+        global run
+        print("Closed down Socket", code, reason)
+        run = False
+
+    def received_message(self, m):
+        consumer(m, self)
+        if len(m) == 175:
+            self.close(reason='Bye bye')
+
+
+def socket_handler():
+    print("DEBUG: socket_handler thread started")
+    global run
+    try:
+        ws.connect()
+        while run:
+            ws.run_forever()
+            run = False
+    except Exception as e:
+        run = False
+        raise e
+    print("DEBUG: socket_handler thread ended")
 
 
 def main():
+    global ws
+    global s
     initial_checks()
     verify_login_credentials()
     print("going online...")
@@ -91,9 +120,10 @@ def main():
     game_aw_status.Value = "online"
     print("running...")
     print(CI.ExecuteCaos("enum 1 2 14 mesg writ targ 1005 mesg writ targ 500 next").Content)
-    socketio_thread = threading.Thread(target=socketio_worker)
-    socketio_thread.daemon = True
-    socketio_thread.start()
+    ws = AwSocketClient(cfg['websocket_url'])
+    socket_handler_thread = threading.Thread(target=socket_handler)
+    socket_handler_thread.daemon = True
+    socket_handler_thread.start()
     # creatures download handling
     creature_download_handler_thread = threading.Thread(target=creature_download_handler)
     creature_download_handler_thread.daemon = True
@@ -114,10 +144,6 @@ def main():
     contactlist_handler_thread = threading.Thread(target=contactlist_handler)
     contactlist_handler_thread.daemon = True
     contactlist_handler_thread.start()
-    # rtdma send handling
-    rtdma_send_handler_thread = threading.Thread(target=rtdma_send_handler)
-    rtdma_send_handler_thread.daemon = True
-    rtdma_send_handler_thread.start()
     while run:
         dbg_out = CI.ExecuteCaos("DBG: POLL").Content.strip('\x00')
         if dbg_out != "":
@@ -127,7 +153,7 @@ def main():
 
 
 def initial_checks():
-    server_version = "alpha andromeda"
+    server_version = "beta baboon"
     bootstrap_version = "alpha alpaca"
     print('My Creatures directory: "%s"' % cfg['my_creatures_directory'])
     print("DEBUG: Checking Bootstrap version...")
@@ -140,7 +166,7 @@ def initial_checks():
         exit(1)
 
     print("Checking server version...")
-    actual_version = requests.get("%s/version" % cfg['url']).text
+    actual_version = s.get("%s/version" % cfg['url']).text
     if actual_version != server_version:
         print("ERROR: Server and Client Version mismatch, are you running the latest/correct client version?")
         print('ERROR: Server Version does not match! Expected "%s", found "%s"' % (server_version, actual_version))
@@ -149,11 +175,12 @@ def initial_checks():
 
 def verify_login_credentials():
     global auth_token
-    auth_test = requests.get("%s/login" % cfg['url'], auth=HTTPBasicAuth(cfg['username'], cfg['password']))
+    auth_test = s.post("%s/auth" % cfg['url'], json={"username": cfg['username'], "password": cfg['password']})
     while auth_test.status_code != 200:
         cfg['username'] = input('Please enter your username: ')
         cfg['password'] = input('Please enter your password: ')
-        auth_test = requests.get("%s/login" % cfg['url'], auth=HTTPBasicAuth(cfg['username'], cfg['password']))
+        auth_test = s.post("%s/auth" % cfg['url'],
+                           json={"username": cfg['username'], "password": cfg['password']})
         if auth_test.status_code != 200:
             print('Wrong username or password, please try again!')
     auth_token = auth_test.json()['token']
@@ -180,8 +207,8 @@ def send_creature(agent):
         with open(creature_file, 'rb') as f:
             files = {'file': f}
             values = {'recipient': tmp['aw_recipient'], 'creature_name': tmp['creature_name']}
-            result = requests.post("%s/creature_upload" % cfg['url'], files=files, data=values,
-                                   headers={'token': auth_token})
+            result = s.post("%s/creature" % cfg['url'], files=files, data=values,
+                            headers={'token': auth_token})
         if result.status_code == 200:
             print("uploaded creature %s to %s" % (tmp['moniker'], tmp['aw_recipient']))
             time.sleep(5)
@@ -189,10 +216,11 @@ def send_creature(agent):
             if not os.path.isfile(creature_file):
                 print('Deleted creature file "%s" after succesfull upload' % creature_file)
             else:
-                print('COULD NOT delete creature file "%s" after succesfull upload, probably already deleted!' % creature_file)
+                print(
+                    'COULD NOT delete creature file "%s" after successful upload!' % creature_file)
         else:
             print("ERROR: uploading creature %s to %s FAILED! Status Code: %s" % (
-            tmp['moniker'], tmp['aw_recipient'], result.status_code))
+                tmp['moniker'], tmp['aw_recipient'], result.status_code))
     else:
         print("ERROR: Could not find %s" % creature_file)
     agent.Kill()
@@ -200,21 +228,21 @@ def send_creature(agent):
 
 @retry(Exception)
 def download_creatures():
-    #print("DEBUG: requesting available creatures")
-    available_creatures = requests.get("%s/creatures" % cfg['url'],
-                                       headers={'token': auth_token})
+    # print("DEBUG: requesting available creatures")
+    available_creatures = s.get("%s/creature" % cfg['url'],
+                                headers={'token': auth_token})
     if available_creatures.status_code == 200:
         for creature in available_creatures.json()['creatures']:
             print("found creature %s" % creature['filename'])
-            result = requests.get("%s/creature/%s" % (cfg['url'], creature['id']),
-                                  headers={'token': auth_token})
+            result = s.get("%s/creature/%s" % (cfg['url'], creature['id']),
+                           headers={'token': auth_token})
             if result.status_code == 200:
                 with open(os.path.join(cfg['my_creatures_directory'], creature['filename']), 'wb') as file:
                     file.write(result.content)
             else:
                 raise Exception("Creature upload Failed! Status Code: %s" % result.status_code)
-            requests.delete("%s/creature/%s" % (cfg['url'], creature['id']),
-                            headers={'token': auth_token})
+            s.delete("%s/creature/%s" % (cfg['url'], creature['id']),
+                     headers={'token': auth_token})
     else:
         print("ERROR: could not request available creatures. Status Code: %s" % available_creatures.status_code)
 
@@ -257,9 +285,9 @@ def send_dma(agent):
     tmp = agent.dict
     print("DETECTED outgoing DMA: %s" % agent.unid)
     print("DEBUG: Processing %s" % tmp)
-    result = requests.post("%s/messages" % cfg['url'],
-                           headers={'token': auth_token},
-                           json=tmp)
+    result = s.post("%s/message" % cfg['url'],
+                    headers={'token': auth_token},
+                    json=tmp)
     if result.status_code == 200:
         print("SENT DMA (%s)" % agent.unid)
         agent.Kill()
@@ -286,11 +314,11 @@ def dma_send_handler():
 
 @retry(Exception)
 def receive_dmas():
-    #print("DEBUG: requesting available DMA's")
-    available_messages = requests.get("%s/messages" % cfg['url'], headers={'token': auth_token})
+    # print("DEBUG: requesting available DMA's")
+    available_messages = s.get("%s/message" % cfg['url'], headers={'token': auth_token})
     if available_messages.status_code == 200:
         for message_id in available_messages.json()['messages']:
-            message = requests.get("%s/message/%s" % (cfg['url'], message_id), headers={'token': auth_token}).json()
+            message = s.get("%s/message/%s" % (cfg['url'], message_id), headers={'token': auth_token}).json()
             CI.ExecuteCaos("enum 1 2 14 mesg writ targ 1004 next")
             try:
                 if AgentBuilder(1, 1, 35754, message).inject().Success:
@@ -298,8 +326,8 @@ def receive_dmas():
             except Exception as e:
                 print("ERROR: %s" % e)
             finally:
-                if not requests.delete("%s/message/%s" % (cfg['url'], message_id),
-                                       headers={'token': auth_token}).status_code == 200:
+                if not s.delete("%s/message/%s" % (cfg['url'], message_id),
+                                headers={'token': auth_token}).status_code == 200:
                     print("ERROR: could not delete message %s" % message)
             CI.ExecuteCaos("enum 1 2 14 mesg writ targ 1005 next")
 
@@ -320,11 +348,34 @@ def dma_receive_handler():
 
 
 @retry(Exception)
+def update_contact_list():
+    users = s.get("%s/user" % cfg['url'], headers={'token': auth_token})
+    for user, status in users.json():
+        # print("DEBUG: %s is %s " % (user, status))
+        add_user_to_contact_list(user)
+        CI.ExecuteCaos('sets game "%s_status" "%s"' % (user, status))
+
+
+def contactlist_handler():
+    print("DEBUG: contactlist_handler thread started")
+    global run
+    while run:
+        try:
+            update_contact_list()
+        except Exception as e:
+            print("ERROR: %s" % e)
+            run = False
+            raise e
+        sleep_while_run(5)
+    print("DEBUG: contactlist_handler thread ended")
+
+
+@retry(Exception)
 def send_rtdma(agent):
     tmp = agent.dict
     print("DETECTED outgoing RTDMA: %s" % agent.unid)
     print("DEBUG: Processing %s" % tmp)
-    socketIO.emit('rtdma', tmp)
+    ws.send(json.dumps(tmp))
     print("SENT RTDMA (%s)" % agent.unid)
     agent.Kill()
 
@@ -346,29 +397,6 @@ def rtdma_send_handler():
             raise e
         sleep_while_run(2)
     print("DEBUG: rtdma_send_handler thread ended")
-
-
-@retry(Exception)
-def update_contact_list():
-    users = requests.get("%s/users" % cfg['url'], headers={'token': auth_token})
-    for user, status in users.json():
-        #print("DEBUG: %s is %s " % (user, status))
-        add_user_to_contact_list(user)
-        CI.ExecuteCaos('sets game "%s_status" "%s"' % (user, status))
-
-
-def contactlist_handler():
-    print("DEBUG: contactlist_handler thread started")
-    global run
-    while run:
-        try:
-            update_contact_list()
-        except Exception as e:
-            print("ERROR: %s" % e)
-            run = False
-            raise e
-        sleep_while_run(5)
-    print("DEBUG: contactlist_handler thread ended")
 
 
 if __name__ == '__main__':
